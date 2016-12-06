30d8e19cbed11ffb9c0321a40c6672e85a4cd8cb
shefman <shefman@mail.ru>
2016-12-06 19:46:26 +0200

    Lab12 ex2

---
+++ BankPrj/src/com/mybank/gui/ATMClient.java
@@ -0,0 +1,100 @@
+package com.mybank.gui;
+
+import com.mybank.data.*;
+import java.io.*;
+import java.awt.*;
+import javax.swing.*;
+
+public class ATMClient {
+
+  private static final String USAGE
+    = "USAGE: java com.mybank.gui.ATMClient <dataFilePath>";
+  
+  private JTextArea output;
+  private JTextField message;
+  private JTextField dataEntry;
+  private JButton displayButton;
+  private JButton depositButton;
+  private JButton withdrawButton;
+  
+  public ATMClient(){
+	  message = new JTextField(75);
+	  message.setEnabled(false);
+	  
+	  dataEntry = new JTextField(10);
+	  dataEntry.setEnabled(false);
+	  
+	  output = new JTextArea(10, 75);
+	  output.setEnabled(false);
+	  
+	  displayButton = new JButton("display");
+	  depositButton = new JButton("deposit");
+	  withdrawButton = new JButton("withdraw");
+  
+}
+  
+  public void launchFrame(){
+		JFrame frame = new JFrame("First Java Bank ATM");
+		
+		frame.setLayout(new BorderLayout());
+
+		frame.add(output, BorderLayout.EAST);
+		frame.add(message, BorderLayout.SOUTH);
+
+		
+		JPanel actionButtonPanel = new JPanel();
+		actionButtonPanel.setLayout(new GridLayout(4, 1));
+		actionButtonPanel.add(displayButton);
+		actionButtonPanel.add(depositButton);
+		actionButtonPanel.add(withdrawButton);
+		actionButtonPanel.add(dataEntry);
+		
+//		frame.add(actionButtonPanel,BorderLayout.CENTER);
+				
+		JPanel keyPadPanel = new JPanel();
+		keyPadPanel.setLayout(new GridLayout(4, 3));
+		for (int i = 1 ; i <= 9 ; i++){
+			keyPadPanel.add(new JButton(String.valueOf(i)));
+		}
+		
+//		frame.add(keyPadPanel,BorderLayout.CENTER);
+		
+		JPanel generalPanel = new JPanel();
+		generalPanel.setLayout(new GridLayout(2, 1));
+		generalPanel.add(actionButtonPanel);
+		generalPanel.add(keyPadPanel);
+		frame.add(generalPanel, BorderLayout.CENTER);
+
+		frame.pack();
+		frame.setVisible(true);
+  }
+ 
+
+  public static void main(String[] args) {
+
+    // Retrieve the dataFilePath command-line argument
+    if ( args.length != 1 ) {
+      System.out.println(USAGE);
+    } else {
+      String dataFilePath = args[0];
+
+      try {
+	System.out.println("Reading data file: " + dataFilePath);
+	// Create the data source and load the Bank data
+	DataSource dataSource = new DataSource(dataFilePath);
+	dataSource.loadData();
+
+	// Run the ATM GUI
+	ATMClient client = new ATMClient();
+	client.launchFrame();
+
+      } catch (IOException ioe) {
+	System.out.println("Could not load the data file.");
+	System.out.println(ioe.getMessage());
+	ioe.printStackTrace(System.err);
+      }
+    }
+  }
+
+  // PLACE YOUR GUI CODE HERE
+}
